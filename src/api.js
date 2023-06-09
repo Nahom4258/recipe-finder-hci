15161e547778f82750473802b1c2bbb6dfa8a257
Nahom Habtamu <nhabtamu42@gmail.com>
2023-06-09 13:23:39 +0300

    remove variable

--- src/api.js
+++ src/api.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const app_id = 'ceaf8b4b'
 const app_key = '67d5dbbd7560a1ef1c8d4e5a23f30aa1'
-const imageSize = 'regular'
 
 const api = axios.create({
   baseURL: `https://api.edamam.com/api/recipes/v2?app_id=${app_id}&app_key=${app_key}&type=public&`, // Set your base URL here
